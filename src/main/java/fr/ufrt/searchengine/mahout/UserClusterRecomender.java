aac69e871200c49bf838a68e3e9477f273cab116
Moditha <moditha88@gmail.com>
2016-03-21 16:38:05 +0100

    User cluseter recommendation

---
+++ src/main/java/fr/ufrt/searchengine/mahout/UserClusterRecomender.java
@@ -0,0 +1,42 @@
+package fr.ufrt.searchengine.mahout;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class UserClusterRecomender {
+
+	public List<RecommendedItem> getRecommendedClusters(int userId) {
+		List<RecommendedItem> recommendations = new ArrayList();
+		try {
+			DataModel model = new FileDataModel(new File("D:\\user-cluster.csv"));
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+
+			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+
+			recommendations = recommender.recommend(userId, 10);
+
+			// writer.close();
+		} catch (TasteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return recommendations;
+	}
+}
