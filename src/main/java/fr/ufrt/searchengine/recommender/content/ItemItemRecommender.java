9a99067a3a1938ed68ddc031dd930150c585aa56
Moditha <modithad@99x.lk>
2016-05-15 12:55:28 +0200

    Getting document names for item similarity and content based item reccomendation

--- src/main/java/fr/ufrt/searchengine/recommender/content/ItemItemRecommender.java
+++ src/main/java/fr/ufrt/searchengine/recommender/content/ItemItemRecommender.java
@@ -13,17 +13,22 @@ import java.util.Map;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import fr.ufrt.searchengine.recommender.Recommender;
 
 public class ItemItemRecommender implements Recommender {
 
-	private final String dirPath = "/Users/larissaleite/Downloads/users_stage3/";
+	private final String dirPath = "D:\\notes\\IR\\users_stage3\\users_stage3\\";
 	
 	@Override
 	public List<String> getRecommendations(List<String> recommendations, int id) {
@@ -59,12 +64,13 @@ public class ItemItemRecommender implements Recommender {
 		}
 		return recommendations;
 	}
-
+	
+	
 	public List<String> getDocumentNames(List<RecommendedItem> recommendedItems) {
 		List<String> documentNames = new ArrayList<String>();
 		FileInputStream fstream;
 		try {
-			fstream = new FileInputStream(dirPath + "docIds.csv");
+			fstream = new FileInputStream(dirPath + "docfinal.csv");
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					fstream));
@@ -74,8 +80,8 @@ public class ItemItemRecommender implements Recommender {
 			Map<Integer, String> m = new HashMap<Integer, String>();
 			
 			while ((strLine = br.readLine()) != null) {
-				String[] arr = strLine.split(",");
-				m.put(Integer.parseInt(arr[0]), arr[1]);
+				String[] arr = strLine.split("\\|");
+				m.put(Integer.parseInt(arr[3]), arr[1]);
 			}
 
 			for (RecommendedItem item : recommendedItems) {
--- src/main/java/fr/ufrt/searchengine/recommender/content/UserKeywordRecommender.java
+++ src/main/java/fr/ufrt/searchengine/recommender/content/UserKeywordRecommender.java
@@ -11,9 +11,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
@@ -93,4 +95,63 @@ public class UserKeywordRecommender implements Recommender {
 		}
 		return clusterNames;
 	}
+
+	public ArrayList<String> getRecommendedDocsItemSimilarity(int userID) {
+		long[] recommendations = null;
+		ArrayList<Long> rec= new ArrayList<>();
+		try {
+			DataModel model = new GenericBooleanPrefDataModel(
+					GenericBooleanPrefDataModel.toDataMap(new FileDataModel(
+							new File(dirPath + "users_documents-keywords.csv"))));
+			UserSimilarity similarity = new LogLikelihoodSimilarity(model);
+			
+			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model); 
+			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity); 
+			recommendations = recommender.mostSimilarUserIDs(userID, 20);
+			
+			for (int i = 0; i < recommendations.length; i++) {
+				if (recommendations[i] > 1000) {
+					rec.add(recommendations[i]);
+				}
+			}
+			
+		} catch (TasteException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return getDocumentNamesForIds(rec);
+	}
+
+	public ArrayList<String> getDocumentNamesForIds(List<Long> recommendedItems) {
+		ArrayList<String> documentNames = new ArrayList<String>();
+		FileInputStream fstream;
+		try {
+			fstream = new FileInputStream(dirPath + "docfinal.csv");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					fstream));
+			
+			String strLine;
+			
+			Map<Integer, String> m = new HashMap<Integer, String>();
+			
+			while ((strLine = br.readLine()) != null) {
+				String[] arr = strLine.split("\\|");
+				m.put(Integer.parseInt(arr[3]), arr[1]);
+			}
+
+			for (Long item : recommendedItems) {
+				String name = m.get((int)(long)item);
+				if (name != null)
+					documentNames.add(name);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return documentNames;
+	}
+
+
 }
